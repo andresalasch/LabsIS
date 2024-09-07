ec935849fc1a8dc578794c848b8ce9de1ad0957b
Andre Salas <andre.salaschinchilla@ucr.ac.cr>
2024-09-06 19:39:12 -0600

    Se termina el laboratorio 5

---
+++ Laboratorio5/Backendlab/Backendlab/Controllers/PaisesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Backendlab.Models;
+using Backendlab.Handlers;
+
+namespace Backendlab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisesHandler _paisesHandler;
+        
+        public PaisesController()
+        {
+            _paisesHandler = new PaisesHandler();
+        }
+
+        [HttpGet] public List<PaisModel> get()
+        {
+            var paises = _paisesHandler.ObtenerPaises();
+            return paises;
+        }
+    }
+}
